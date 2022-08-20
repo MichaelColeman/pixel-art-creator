af028d70b27436b67ad5d22372bcbd7c8b0f7bb4
Tommy <Michael.t.coleman7@gmail.com>
2022-08-19 21:21:36 -0700

    Remove uneeded parenthesis

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function createGrid(sideLength) {
   //find grid container
-  let gridContainer = document.getElementById('gridContainer');
+  let gridContainer = document.querySelector('.js-grid-container');
   //create grid
-  for (let i = 0; i < (sideLength*sideLength); i++) {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("gridItem");
+  for (let i = 0; i < sideLength * sideLength; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('grid-item');
     gridContainer.appendChild(newDiv);
   }
 }
-
 createGrid(16);
