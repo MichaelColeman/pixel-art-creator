5d0d20ff3c6cc6dac4526f62d5ecadd473c0919e
Tommy <Michael.t.coleman7@gmail.com>
2022-08-22 14:04:13 -0700

    Add ability to change canvas pixel density

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function createGrid(sideLength) {
   //find grid container
   let gridContainer = document.querySelector('.js-grid-container');
+
+  //change number of grid columns to be equal to the sideLength
+  gridContainer.style.cssText = `grid-template-columns: repeat(${sideLength}, 1fr)`;
+
   //create grid
   for (let i = 0; i < sideLength * sideLength; i++) {
     const newDiv = document.createElement('div');
