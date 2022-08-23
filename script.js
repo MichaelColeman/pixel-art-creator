6d0cdf13328368e0014347568ec94917971780dd
Tommy <Michael.t.coleman7@gmail.com>
2022-08-22 17:59:24 -0700

    Add functionality to resize button

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ function createGrid(sideLength) {
   //find grid container
   let gridContainer = document.querySelector('.js-grid-container');
 
+  //delete current grid
+  let child = gridContainer.lastElementChild;
+  while (child) {
+    gridContainer.removeChild(child);
+    child = gridContainer.lastChild;
+  }
+
   //change number of grid columns to be equal to the sideLength
   gridContainer.style.cssText = `grid-template-columns: repeat(${sideLength}, 1fr)`;
 
@@ -12,4 +19,21 @@ function createGrid(sideLength) {
     gridContainer.appendChild(newDiv);
   }
 }
+
+function getGridLength() {
+  const userChoice = prompt('enter size of new grid');
+  return userChoice;
+}
+
+function resizeGrid() {
+  const sideLength = getGridLength();
+  createGrid(sideLength);
+}
+
+const resizeGridButton = document.querySelector('.js-resize-canvas');
+resizeGridButton.addEventListener('click', function (e) {
+  resizeGrid();
+  console.log(`yo ${e.target}`);
+});
+
 createGrid(16);
